bdd469cc4d799feca0932ea7a05df1bc9db01179
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-10-25 08:30:53 +0800

    add files

--- linearEquation.py
+++ linearEquation.py
@@ -1,9 +1,9 @@
 from sympy import symbols, Eq, solve
 
-x, y, z = symbols('x,y,z')
-
-eq1 = Eq((-6*x-4*y+3*z), 7)
-eq2 = Eq((-2*x+5*y+z), 15)
-eq3 = Eq((x-5*y+6*z), -19)
-  
-print(solve((eq1, eq2, eq3), (x, y, z)))
+x, y= symbols("x, y")
+eq1 = Eq((x+y), 1)
+print(eq1)
+eq2 = Eq((x-y), 1)
+print(eq2)
+solving = solve((eq1, eq2), (x,y))
+print(solving)
