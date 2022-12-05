be78ed68629620c8b05e93ccb4ac47b1edbabadf
kamhar-ngado <kamhar.ngado.id@gmail.com>
2022-12-06 01:25:03 +0800

    add files

--- linearEquation.py
+++ linearEquation.py
@@ -1,12 +1,15 @@
 from sympy import symbols, Eq, solve
+import numpy as np
 
-x, y, z= symbols("x, y, z")
-eq1 = Eq((x+y+z), 1)
+x,y = symbols("x,y")
+eq1= Eq((2*x+3*y), 16)
 print(eq1)
-eq2 = Eq((3*x+2*y+z), 2)
+eq2= Eq((x-2*y), 1)
 print(eq2)
-eq3 = Eq((4*x-2*y-3*z), 3)
-print(eq3)
-solving = solve((eq1, eq2, eq3), (x,y,z))
+solving = solve((eq1,eq2),(x,y))
 print(solving)
 
+A = np.array([[2, 3], [1, -2]])
+B = np.array([16, 1])
+X = np.linalg.solve(A,B)
+print(X)
